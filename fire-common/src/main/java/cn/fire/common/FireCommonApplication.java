01920fc437ccbb9be7ecce90dca1b31318619715
wangzhichao <wangzhichao03@tojoy.com>
2020-07-28 17:05:29 +0800

    update

---
+++ fire-common/src/main/java/cn/fire/common/FireCommonApplication.java
@@ -0,0 +1,13 @@
+package cn.fire.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireCommonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireCommonApplication.class, args);
+    }
+
+}
---
+++ fire-common/src/test/java/cn/fire/common/FireCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fire.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
