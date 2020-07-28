85e1279496b6c5ca485f2e123d7be76ddbddd92d
wangzhichao <wangzhichao03@tojoy.com>
2020-07-28 17:37:28 +0800

    update

---
+++ fire-jobs/src/main/java/cn/fire/jobs/FireJobsApplication.java
@@ -0,0 +1,13 @@
+package cn.fire.jobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireJobsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FireJobsApplication.class, args);
+    }
+
+}
---
+++ fire-jobs/src/test/java/cn/fire/jobs/FireJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.fire.jobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
