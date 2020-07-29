9e2b5e370826ea6bf7a223c519980bc8adc1d80e
wangzhichao <wangzhichao03@tojoy.com>
2020-07-29 16:32:20 +0800

    update

--- fire-common-web/src/main/java/cn/fire/common/web/core/R.java
+++ fire-common-web/src/main/java/cn/fire/common/web/core/R.java
@@ -9,53 +9,60 @@ import java.io.Serializable;
  * @Date: 2020/7/29 15:00
  */
 
-public class Response<T> implements Serializable {
+public class R<T> implements Serializable {
 
     private static final String OK = "ok";
-    private static final String ERROR = "error";
+    private static final String ERROR = "deny";
 
     @Getter
     private Meta meta;
     @Getter
-    private T t;
+    private T data;
 
-    public Response(boolean success,Integer code,String msg) {
+    public R(boolean success, Integer code, String msg) {
         this.meta = new Meta(success,code,msg);
     }
 
-    public Response(T t) {
+    public R(boolean success,String msg) {
+        this.meta = new Meta(success,msg);
+    }
+
+    public R() {
         this.meta = new Meta(true,OK);
-        this.t = t;
     }
 
-
-
-    public Response ok() {
+    public R(T data) {
         this.meta = new Meta(true,OK);
-        return this;
+        this.data = data;
     }
 
-    public Response ok(T t) {
-        this.meta = new Meta(true,OK);
-        this.t = t;
-        return this;
+    /**
+     * Response
+     * @return
+     */
+    public static R ok() {
+        return new R();
     }
 
-    public Response deny() {
-        this.meta = new Meta(false,ERROR);
-        return this;
+    public static R ok(String message) {
+        return new R(true,message);
     }
 
-    public Response deny(String message) {
-        this.meta = new Meta(false,message);
-        return this;
+    public static <T> R<T> ok(T data) {
+        return new R(data);
     }
 
-    public Response deny(Integer code,String message) {
-        this.meta = new Meta(false,code,message);
-        return this;
+    public static R fail() {
+        return new R(false,ERROR);
     }
 
+    public static R fail(String message) {
+        return new R(false,message);
+    }
+
+    public static R fail(Integer code, String message) {
+        return new R(false,code,message);
+    }
 
     public class Meta {
 
