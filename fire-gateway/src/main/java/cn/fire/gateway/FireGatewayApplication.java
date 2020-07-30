41df7d989f6159e6a07fbf3bb545b6ee7741841b
wangzhichao <wangzhichao03@tojoy.com>
2020-07-30 15:30:47 +0800

    gitway nacos

--- fire-gateway/src/main/java/cn/fire/gateway/FireGatewayApplication.java
+++ fire-gateway/src/main/java/cn/fire/gateway/FireGatewayApplication.java
@@ -3,6 +3,7 @@ package cn.fire.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class FireGatewayApplication {
 
