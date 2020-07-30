20f51787dc3992d2a972f0f8f6ee7e06f742b14c
wangzhichao <wangzhichao03@tojoy.com>
2020-07-30 16:05:42 +0800

    update

--- fire-oauth/src/main/java/cn/fire/oauth/config/AuthServerConfig.java
+++ fire-oauth/src/main/java/cn/fire/oauth/config/AuthServerConfig.java
@@ -2,14 +2,11 @@ package cn.fire.oauth.config;
 
 import cn.fire.oauth.config.enhancer.JwtTokenEnhancer;
 import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
