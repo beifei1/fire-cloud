988ab268504bd523bacd30263f58932a3e5176c0
wangzhichao <wangzhichao03@tojoy.com>
2020-07-30 17:37:04 +0800

    update

--- fire-user/fire-user-consumer/src/main/java/cn/fire/user/FireUserConsumerApplication.java
+++ fire-user/fire-user-consumer/src/main/java/cn/fire/user/FireUserConsumerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@SpringBootApplication(scanBasePackages = {"cn.fire.common.web"})
+@SpringBootApplication(scanBasePackages = {"cn.fire.common.web","cn.fire.user"})
 public class FireUserConsumerApplication {
 
     public static void main(String[] args) {
