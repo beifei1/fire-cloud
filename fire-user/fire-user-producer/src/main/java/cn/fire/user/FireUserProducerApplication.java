928dcb3e288e0551083f8514fc0effc6d80b1fd6
wangzhichao <wangzhichao03@tojoy.com>
2020-07-31 16:14:56 +0800

    update

--- fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
+++ fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
@@ -3,6 +3,7 @@ package cn.fire.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +13,13 @@ public class FireUserProducerApplication {
         SpringApplication.run(FireUserProducerApplication.class, args);
     }
 
+//    @Value("${redis.name}")
+//    private String name;
+//
+//    @Bean
+//    CommandLineRunner run () {
+//        return (arg) -> {
+//            System.out.println("==================:" + name);
+//        };
+//    }
 }
