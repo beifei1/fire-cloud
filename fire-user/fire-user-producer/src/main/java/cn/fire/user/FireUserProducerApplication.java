893786d26314c506dc6b3c2a6a96a36ca4027917
wangzhichao <wangzhichao03@tojoy.com>
2020-07-31 17:41:01 +0800

    update

--- fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
+++ fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
@@ -1,9 +1,9 @@
 package cn.fire.user;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "cn.fire.user")
