f34e09adb924e95348a9237a4041e138d6f24650
wangzhichao <wangzhichao03@tojoy.com>
2020-08-03 09:21:27 +0800

    update

--- fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
+++ fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
@@ -1,9 +1,11 @@
 package cn.fire.user;
 
-import org.mybatis.spring.annotation.MapperScan;
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = "cn.fire.user")
@@ -13,7 +15,7 @@ public class FireUserProducerApplication {
         SpringApplication.run(FireUserProducerApplication.class, args);
     }
 
-//    @Value("${redis.name}")
+//    @NacosValue("${redis.name}")
 //    private String name;
 //
 //    @Bean
