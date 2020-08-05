099a7d26adae8394725ed6e6b8efb251ada93f03
wangzhichao <wangzhichao03@tojoy.com>
2020-08-05 15:31:00 +0800

    集成Knife4j

--- fire-common-web/src/main/java/cn/fire/common/web/core/Request.java
+++ fire-common-web/src/main/java/cn/fire/common/web/core/Request.java
@@ -4,6 +4,9 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+
 /**
  * @Author: wangzc
  * @Date: 2020/8/4 11:04
@@ -13,9 +16,13 @@ import lombok.Data;
 @ApiModel(description = "统一请求体Wrapper")
 public class Request<T> {
 
+    @Valid
+    @NotBlank(message = "数据签名不能为空")
     @ApiModelProperty("数据签名")
     private String sign;
 
+    @Valid
+    @NotBlank(message = "时间戳不能为空")
     @ApiModelProperty("时间戳")
     private String timestamp;
 
--- fire-common-web/src/main/java/cn/fire/common/web/handler/CommonExceptionHandler.java
+++ fire-common-web/src/main/java/cn/fire/common/web/handler/CommonExceptionHandler.java
@@ -2,11 +2,16 @@ package cn.fire.common.web.handler;
 
 import cn.fire.common.exception.BaseException;
 import cn.fire.common.web.core.R;
+import com.google.common.collect.Lists;
+import org.apache.tomcat.util.buf.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
 
 /**
  * @Author: wangzc
@@ -24,6 +29,21 @@ public class CommonExceptionHandler {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public R missingParameterException(MissingServletRequestParameterException e) {
         return R.fail(e.getMessage());
+    }
+
+    /**
+     * 参数验证异常
+     * @param e
+     * @return
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public R methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        java.util.List<String> msgs = Lists.newArrayList();
+        e.getBindingResult().getFieldErrors().stream().forEach(field -> {
+            msgs.add(field.getDefaultMessage());
+        });
+        return R.fail(StringUtils.join(msgs,','));
     }
 
     /**
