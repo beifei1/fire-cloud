722caac146afa669797210059b51343a9fb0a004
wangzhichao <wangzhichao03@tojoy.com>
2020-08-05 10:50:55 +0800

    open api

--- fire-gateway/src/main/java/cn/fire/gateway/FireGatewayApplication.java
+++ fire-gateway/src/main/java/cn/fire/gateway/FireGatewayApplication.java
@@ -1,19 +1,10 @@
 package cn.fire.gateway;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-@OpenAPIDefinition(
-        info = @Info(
-                title = "fire cloud microservice",
-                version = "1.0",
-                description = "fire cloud microservice demo api"
-        )
-)
 public class FireGatewayApplication {
 
     public static void main(String[] args) {
