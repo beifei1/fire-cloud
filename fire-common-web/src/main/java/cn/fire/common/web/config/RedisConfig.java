1a7b87fe86b769eabdfd66385dda3ba815ce27fe
wangzhichao <wangzhichao03@tojoy.com>
2020-08-21 10:22:34 +0800

    集成redis

--- fire-common-web/src/main/java/cn/fire/common/web/config/RedisConfig.java
+++ fire-common-web/src/main/java/cn/fire/common/web/config/RedisConfig.java
@@ -1,42 +1,76 @@
 package cn.fire.common.web.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cache.Cache;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+import javax.annotation.Resource;
 import java.io.Serializable;
+import java.time.Duration;
 
 /**
  * @Author: wangzc
  * @Date: 2020/8/21 9:44
  */
 
+@Slf4j
 @Configuration
 @ConditionalOnProperty(name = "spring.redis.host")
 public class RedisConfig extends CachingConfigurerSupport {
 
-    @Autowired
-    RedisConnectionFactory redisConnectionFactory;
+    @Resource
+    private RedisConnectionFactory redisConnectionFactory;
 
-
-    /**
-     * 指定redis template序列化方式
-     * @return
-     */
     @Bean
     public RedisTemplate<String, Serializable> redisTemplate(){
         RedisTemplate<String, Serializable> redisTemplate = new RedisTemplate<>();
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         return redisTemplate;
     }
 
+    @Bean
+    @Override
+    public CacheManager cacheManager() {
+        return RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(RedisCacheConfiguration.defaultCacheConfig().entryTtl(Duration.ofDays(30))).build();
+    }
 
+    @Bean
+    @Override
+    public CacheErrorHandler errorHandler() {
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.error("Redis occur handleCacheGetError：key -> [{}]", key, e);
+            }
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.error("Redis occur handleCachePutError：key -> [{}]；value -> [{}]", key, value, e);
+            }
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key)    {
+                log.error("Redis occur handleCacheEvictError：key -> [{}]", key, e);
+            }
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.error("Redis occur handleCacheClearError：", e);
+            }
+        };
+        return cacheErrorHandler;
+    }
 }
