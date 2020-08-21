a7d1b7c7f200f3d6eea0d454e34182e226868793
wangzhichao <wangzhichao03@tojoy.com>
2020-08-21 09:26:40 +0800

    update

--- fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
+++ fire-user/fire-user-producer/src/main/java/cn/fire/user/FireUserProducerApplication.java
@@ -2,9 +2,11 @@ package cn.fire.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
+@EnableCaching
 @SpringBootApplication(scanBasePackages = {"cn.fire.user","cn.fire.common.web"})
 public class FireUserProducerApplication {
 
