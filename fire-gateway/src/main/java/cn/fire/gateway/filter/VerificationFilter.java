5dd9c089c433e845b12cbcae6c3def85001e65c0
wangzhichao <wangzhichao03@tojoy.com>
2020-08-21 11:16:06 +0800

    集成redis

---
+++ fire-gateway/src/main/java/cn/fire/gateway/filter/VerificationFilter.java
@@ -0,0 +1,31 @@
+package cn.fire.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @Author: wangzc
+ * @Date: 2020/8/21 11:12
+ */
+@Slf4j
+@Component
+@ConditionalOnProperty(name = "gateway.verification.enable",value = "true")
+public class VerificationFilter implements GlobalFilter, Ordered {
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
