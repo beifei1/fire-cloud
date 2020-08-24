9ff64979b55cc2c3d49baa441bc9cd28c300cc7c
wangzhichao <wangzhichao03@tojoy.com>
2020-08-24 16:19:38 +0800

    update

--- fire-common-web/src/main/java/cn/fire/common/web/config/GlobalJacksonConfig.java
+++ fire-common-web/src/main/java/cn/fire/common/web/config/GlobalJacksonConfig.java
@@ -33,6 +33,7 @@ public class GlobalJacksonConfig {
             builder.serializerByType(Integer.TYPE, new DefaultToStringSerializer());
             builder.serializerByType(Double.TYPE, new DefaultToStringSerializer());
             builder.serializerByType(Float.TYPE, new DefaultToStringSerializer());
+            builder.serializerByType(Short.TYPE, new DefaultToStringSerializer());
             builder.serializerByType(Boolean.TYPE, new DefaultToStringSerializer());
         };
     }
