5887d8f572f25f1ea87cfbc9e671640a2b664269
wangzhichao <wangzhichao03@tojoy.com>
2020-08-26 11:08:59 +0800

    add arg resolver

--- fire-common-web/src/main/java/cn/fire/common/web/handler/GlobalUserResolver.java
+++ fire-common-web/src/main/java/cn/fire/common/web/handler/GlobalUserResolver.java
@@ -1,7 +1,11 @@
 package cn.fire.common.web.handler;
 
 import cn.fire.common.web.core.request.JUser;
+import com.alibaba.fastjson.JSONObject;
+import io.micrometer.core.instrument.util.StringUtils;
 import org.springframework.core.MethodParameter;
+import org.springframework.security.jwt.Jwt;
+import org.springframework.security.jwt.JwtHelper;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -16,8 +20,7 @@ public class GlobalUserResolver implements HandlerMethodArgumentResolver {
     @Override
     public boolean supportsParameter(MethodParameter param) {
 
-        if (param.getParameterType().isAssignableFrom(JUser.class) &&
-                param.hasParameterAnnotation(cn.fire.common.web.anno.UserProfile.class)) {
+        if (param.getParameterType().isAssignableFrom(JUser.class) && param.hasParameterAnnotation(cn.fire.common.web.anno.UserProfile.class)) {
             return Boolean.TRUE;
         }
         return Boolean.FALSE;
@@ -28,12 +31,22 @@ public class GlobalUserResolver implements HandlerMethodArgumentResolver {
                                   ModelAndViewContainer container,
                                   NativeWebRequest request,
                                   WebDataBinderFactory binderFactory) throws Exception {
-        JUser profile = new JUser();
-        profile.setGender(1);
-        profile.setMobile("18631431162");
-        profile.setUserId(1L);
-        profile.setUserName("王志超");
 
-        return profile;
+        String authorization = StringUtils.isBlank(request.getHeader("Authorization")) ? null : request.getHeader("Authorization").replace("Bearer", "").trim();
+
+        if (StringUtils.isNotBlank(authorization)) {
+            Jwt jwt = JwtHelper.decode(authorization);
+            if (jwt != null) {
+                JSONObject jsonClaims = JSONObject.parseObject(jwt.getClaims());
+                JUser profile = new JUser();
+                profile.setGender(jsonClaims.getInteger("gender"));
+                profile.setMobile(jsonClaims.getString("mobile"));
+                profile.setUserId(jsonClaims.getLong("user_id"));
+                profile.setUserName(jsonClaims.getString("user_name"));
+
+                return profile;
+            }
+        }
+        return null;
     }
 }
