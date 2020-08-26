9a603f1456c1776e043aab5d0f15c0e8719b0d76
wangzhichao <wangzhichao03@tojoy.com>
2020-08-26 09:21:16 +0800

    delete LICENSE

--- fire-oauth/src/main/java/cn/fire/oauth/config/enhancer/JwtTokenEnhancer.java
+++ fire-oauth/src/main/java/cn/fire/oauth/config/enhancer/JwtTokenEnhancer.java
@@ -1,6 +1,8 @@
 package cn.fire.oauth.config.enhancer;
 
+import cn.fire.oauth.pojo.dto.UserDTO;
 import com.google.common.collect.Maps;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -19,9 +21,11 @@ public class JwtTokenEnhancer implements TokenEnhancer {
     @Override
     public OAuth2AccessToken enhance(OAuth2AccessToken oAuth2AccessToken, OAuth2Authentication oAuth2Authentication) {
 
+        UserDTO user = (UserDTO)oAuth2Authentication.getUserAuthentication().getPrincipal();
         Map<String,Object> echance = Maps.newHashMap();
-        echance.put("extension","jwt tuozhanxinxi");
-
+        echance.put("userName", user.getUsername());
+        echance.put("userId", user.getId());
+        echance.put("mobile", user.getMobile());
         ((DefaultOAuth2AccessToken)oAuth2AccessToken).setAdditionalInformation(echance);
 
         return oAuth2AccessToken;
