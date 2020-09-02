06d68724eda2d05d396b6fe4f9c799476fdff953
wangzhichao <wangzhichao03@tojoy.com>
2020-09-02 13:30:21 +0800

    更新文档加载方式

--- fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/SwaggerConfig.java
+++ fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/SwaggerConfig.java
@@ -1,6 +1,5 @@
 package cn.fire.user.config;
 
-import cn.fire.common.exception.BaseException;
 import cn.fire.common.web.config.AbstractSwaggerConfig;
 import cn.fire.user.api.exception.UserException;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
@@ -10,9 +9,7 @@ import springfox.documentation.service.*;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * @Author: wangzc
@@ -23,13 +20,8 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig extends AbstractSwaggerConfig {
 
-    private static List<ResponseMessage> codes = new ArrayList<>();
-
     @PostConstruct
     void init () {
-        Arrays.stream(BaseException.BaseErrorEnum.values()).forEach(em -> {
-            codes.add(new ResponseMessageBuilder().code(em.getCode()).message(em.getDescription()).build());
-        });
         Arrays.stream(UserException.ErrorEnum.values()).forEach(em -> {
             codes.add(new ResponseMessageBuilder().code(em.getCode()).message(em.getDescription()).build());
         });
