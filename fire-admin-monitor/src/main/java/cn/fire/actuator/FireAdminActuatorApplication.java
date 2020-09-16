4f69fe9621bb7333c477d414bb72fafba109188c
wangzhichao <wangzhichao03@tojoy.com>
2020-09-16 09:11:16 +0800

    update

--- fire-admin-monitor/src/main/java/cn/fire/actuator/FireAdminActuatorApplication.java
+++ fire-admin-monitor/src/main/java/cn/fire/actuator/FireAdminActuatorApplication.java
@@ -3,8 +3,10 @@ package cn.fire.actuator;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FireAdminActuatorApplication {
 
