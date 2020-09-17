249753e0299ec0a71b054d7d66606d056b6e1713
wangzhichao <wangzhichao03@tojoy.com>
2020-09-17 11:22:11 +0800

    Distributed lock impl

--- fire-common-web/src/main/java/cn/fire/common/web/lock/impl/ZookeeperDistributedLock.java
+++ fire-common-web/src/main/java/cn/fire/common/web/lock/impl/ZookeeperDistributedLock.java
@@ -1,33 +1,19 @@
 package cn.fire.common.web.lock.impl;
 
 import cn.fire.common.web.lock.DistributedLock;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 
 import java.util.concurrent.TimeUnit;
 import java.util.function.Supplier;
 
 /**
  * @Author: wangzc
- * @Date: 2020/9/17 11:11
+ * @Date: 2020/9/17 11:20
  */
-@Primary
-@Component
-@ConditionalOnBean(RedissonClient.class)
-public class RedisDistributeLock implements DistributedLock {
-
-    @Autowired
-    private RedissonClient redissonClient;
-
+//TODO zookeeper distributed lock
+public class ZookeeperDistributedLock implements DistributedLock {
     @Override
     public <T> T lock(String key, Supplier<T> success, Supplier<T> failure) {
-//        RLock lock = redissonClient.getLock(key);
-//        lock.lock();
-        return success.get();
+        return null;
     }
 
     @Override
