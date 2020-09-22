30c8528dab7522c08d3334d3e7d3989f5658d9ea
wangzhichao <wangzhichao03@tojoy.com>
2020-09-22 16:16:17 +0800

    参数注入

--- fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/WebMvcConfig.java
+++ fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/WebMvcConfig.java
@@ -2,7 +2,6 @@ package cn.fire.user.config;
 
 import cn.fire.common.web.core.request.JUser;
 import cn.fire.common.web.handler.GlobalUserProfileResolver;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,24 +9,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 /**
- * 注入参数解析器，在header中取出用户信息
- *
- * @see cn.fire.user.controller.UserController#detail(JUser)
- *
+ * Spring WebMvcConfig
  * @Author: wangzc
  * @Date: 2020/8/26 10:25
  */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    /**
+     * 在header中取出用户信息, 并注入到方法参数中
+     * @see cn.fire.user.controller.UserController#detail(JUser)
+     */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-        resolvers.add(globalUserResolver());
-    }
-
-    @Bean
-    GlobalUserProfileResolver globalUserResolver() {
-        return new GlobalUserProfileResolver();
+        resolvers.add(new GlobalUserProfileResolver());
     }
 
 }
