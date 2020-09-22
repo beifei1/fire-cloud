bd64f3381f6197a641cd132a69c35bb8d8559793
wangzhichao <wangzhichao03@tojoy.com>
2020-09-22 16:14:11 +0800

    加入注释

--- fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/WebMvcConfig.java
+++ fire-module-user/fire-user-consumer/src/main/java/cn/fire/user/config/WebMvcConfig.java
@@ -1,5 +1,6 @@
 package cn.fire.user.config;
 
+import cn.fire.common.web.core.request.JUser;
 import cn.fire.common.web.handler.GlobalUserProfileResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 /**
+ * 注入参数解析器，在header中取出用户信息
+ *
+ * @see cn.fire.user.controller.UserController#detail(JUser)
+ *
  * @Author: wangzc
  * @Date: 2020/8/26 10:25
  */
