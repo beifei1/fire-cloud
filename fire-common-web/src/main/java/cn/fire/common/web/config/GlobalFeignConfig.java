12ce1ff7fac9d27f9288caff0bc10f9e3e5722eb
wangzhichao <wangzhichao03@tojoy.com>
2020-09-27 10:09:33 +0800

    删除consumer和producer间通信的基础验证

--- fire-common-web/src/main/java/cn/fire/common/web/config/GlobalFeignConfig.java
+++ fire-common-web/src/main/java/cn/fire/common/web/config/GlobalFeignConfig.java
@@ -2,11 +2,9 @@ package cn.fire.common.web.config;
 
 import cn.fire.common.web.handler.ExceptionFeignDecoder;
 import feign.*;
-import feign.auth.BasicAuthRequestInterceptor;
 import feign.codec.Decoder;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
@@ -20,12 +18,6 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ConditionalOnClass(feign.Retryer.class)
 public class GlobalFeignConfig {
-
-    @Value("${spring.security.user.name:}")
-    private String httpBasicUserName;
-
-    @Value("${spring.security.user.password:}")
-    private String httpBasicPassword;
 
     @Autowired
     private ObjectFactory<HttpMessageConverters> objectFactory;
@@ -45,14 +37,14 @@ public class GlobalFeignConfig {
         return new ExceptionFeignDecoder(objectFactory);
     }
 
-    /**
-     * Consumer和Producer间增加基础的Basic认证
-     * @return
-     */
-    @Bean
-    BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
-        return new BasicAuthRequestInterceptor(httpBasicUserName, httpBasicPassword);
-    }
+//    /**
+//     * Consumer和Producer间增加基础的Basic认证
+//     * @return
+//     */
+//    @Bean
+//    BasicAuthRequestInterceptor basicAuthRequestInterceptor() {
+//        return new BasicAuthRequestInterceptor(httpBasicUserName, httpBasicPassword);
+//    }
 
 }
 
