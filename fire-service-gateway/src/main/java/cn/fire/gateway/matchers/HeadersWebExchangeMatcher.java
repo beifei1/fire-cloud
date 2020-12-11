f1d80a5f3b5ceea9573cfeffaae3b9af5a27c973
wangzhichao <wangzhichao03@tojoy.com>
2020-12-11 11:01:08 +0800

    加入http header matcher拦截指定header

--- fire-service-gateway/src/main/java/cn/fire/gateway/matchers/HeadersWebExchangeMatcher.java
+++ fire-service-gateway/src/main/java/cn/fire/gateway/matchers/HeadersWebExchangeMatcher.java
@@ -1,7 +1,9 @@
 package cn.fire.gateway.matchers;
 
+
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -12,8 +14,21 @@ import reactor.core.publisher.Mono;
 
 public class HeadersWebExchangeMatcher implements ServerWebExchangeMatcher {
 
+    private MultiValueMap<String,String> multiValueMap;
+
+    public HeadersWebExchangeMatcher(MultiValueMap<String,String> map) {
+        this.multiValueMap = map;
+    }
+
+    public HeadersWebExchangeMatcher(String k, String v) {
+//        MultiValueMap<String,String> map = Mul
+    }
+
     @Override
-    public Mono<MatchResult> matches(ServerWebExchange serverWebExchange) {
+    public Mono<MatchResult> matches(ServerWebExchange ex) {
+
+//        ex.getRequest().getHeaders().containsKey()
         return null;
     }
+
 }
